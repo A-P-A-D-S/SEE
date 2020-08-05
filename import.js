5785774697b2dd39ec931e51efacd1e7d79e417e
antonio <68173665+A-P-A-D-S@users.noreply.github.com>
2020-08-05 15:17:40 -0300

    Update import.js

--- import.js
+++ import.js
@@ -1,1 +1,3 @@
-import {bootstrap} from "jquery.min.js"
+"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
+;
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
