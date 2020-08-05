94b5c9d28ca24f463185a898bc2445167f3acf5f
antonio <68173665+A-P-A-D-S@users.noreply.github.com>
2020-08-05 15:31:02 -0300

    Update import.js

--- import.js
+++ import.js
@@ -1,3 +1,7 @@
 "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
 ;
 "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+
+"https://code.jquery.com/jquery-3.5.1.min.js"
+
+"https://unpkg.com/@popperjs/core@2"
